e9c4ef26ee0c98a8a8084e8fbc353a9027ea29f6
kemoel.miranda <kemoel.miranda@ailos.coop.br>
2021-12-08 19:39:57 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/ContaController.cs
@@ -0,0 +1,25 @@
+﻿using ByteBank.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ByteBank.Controllers
+{
+    public class ContaController: Controller
+    {
+
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(ContaRegistrarViewModel modelo)
+        {
+            return View();
+        }
+
+    }
+}
